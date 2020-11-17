788cddd02fbca62e64506b9480d8dbef691ef24d
Harshvardhan <harshvardhanvipat@gmail.com>
2020-11-18 09:08:05 +1100

    Updates

---
+++ precache-manifest.4f4a67a3ff7b92bee156aca36cd220f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "456b8369677460d0bfdbb7ffededa398",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "168d5ecae11992535246",
+    "url": "/netflix-clone/static/css/main.1e4419d6.chunk.css"
+  },
+  {
+    "revision": "a2d3e525ffa77c4ad9a8",
+    "url": "/netflix-clone/static/js/2.3ca0194a.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.3ca0194a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "168d5ecae11992535246",
+    "url": "/netflix-clone/static/js/main.f1d926e5.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
